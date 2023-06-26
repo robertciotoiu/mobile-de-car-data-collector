648682faa6b78abc962a83ade08e9282f30625b7
Robert Ciotoiu <robert.ciotoiu@gmail.com>
2023-06-26 19:39:11 +0200

    API-4 implement get all listings and get listing by id endpoints
    getListings implementation is highly efficient as we avoid to use findAll which return a pageable and does a non-indexed count

--- mobile-de-data-api/src/main/java/com/robertciotoiu/controller/ListingController.java
+++ mobile-de-data-api/src/main/java/com/robertciotoiu/controller/ListingController.java
@@ -3,14 +3,10 @@ package com.robertciotoiu.controller;
 import com.robertciotoiu.data.model.raw.Listing;
 import com.robertciotoiu.data.repository.ListingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Slice;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/listings")
@@ -20,10 +16,15 @@ public class ListingController {
     ListingRepository listingRepository;
 
     @GetMapping
-    public Page<Listing> getListings(
+    public Slice<Listing> getListings(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "50") int size) {
+            @RequestParam(defaultValue = "10") int size) {
         Pageable pageable = PageRequest.of(page, size);
-        return listingRepository.findAll(pageable);
+        return listingRepository.findBy(pageable);
+    }
+
+    @GetMapping("/{listingId}")
+    public Listing getListing(@PathVariable String listingId) {
+        return listingRepository.findById(listingId).orElse(null);
     }
 }
